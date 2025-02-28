94c370ef2cb332b5cb6f9a5c68c0e14fd1aa13db
Jonathan <jonathandajome@gmail.com>
2025-02-28 11:01:45 -0500

    Add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold">App</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   );
 };
 
